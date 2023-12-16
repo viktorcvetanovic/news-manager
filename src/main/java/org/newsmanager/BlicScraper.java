3e81d93a258e635e8d0934fdd900e9da2788c6a6
Viktor <viktorcvetanovic@gmail.com>
2023-12-16 01:04:18 +0100

    Scraper is working

--- src/main/java/org/newsmanager/BlicScraper.java
+++ src/main/java/org/newsmanager/BlicScraper.java
@@ -2,22 +2,55 @@ package org.newsmanager;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.newsmanager.data.Post;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BlicScraper implements Scraper {
 
     @Override
     public List<Post> scrape() {
+        List<Post> posts;
         try {
-            Document doc = Jsoup.connect("https://www.blic.rs/vesti").get();
-            System.out.println(doc);
+            Document mainPage = Jsoup.connect("https://www.blic.rs/najnovije-vesti/").get();
+            List<Element> news = mainPage.getElementById("tab-blic-info")
+                    .getElementsByTag("article");
+            System.out.println(news.size());
+            posts = news.stream()
+                    .map(e->{
+                        String link = e.getElementsByClass("news__img")
+                                .get(0)
+                                .getElementsByTag("a")
+                                .get(0)
+                                .attr("href");
+                        String header = "";
+                        String body = "";
+                        try {
+                            Document newsInformation = Jsoup.connect(link).get();
+                            header = newsInformation.getElementsByTag("h1").text();
+                            body =  newsInformation.getElementsByClass("article__text")
+                                    .get(0)
+                                    .getElementsByTag("p")
+                                    .stream()
+                                    .map(Element::text)
+                                    .collect(Collectors.joining(" "));
+
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                        System.out.println(link);
+                        return new Post(header,body);
+                    })
+                    .collect(Collectors.toList());
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return null;
+        return posts;
     }
 }
